fa42069cd01964a32f306eaec4fc54574f1f9907
je-von <jevon.levin@gmail.com>
2023-03-20 17:45:09 +0700

    feat: add StretchPage

---
+++ Baduuut/StretchPage.swift
@@ -0,0 +1,69 @@
+//
+//  StretchPage.swift
+//  Baduuut
+//
+//  Created by Jevon Levin on 20/03/23.
+//
+
+import SwiftUI
+
+struct StretchPage: View {
+    @State private var isTimerVisible: Bool = false
+    var body: some View {
+        
+        ZStack {
+            Circle()
+                .foregroundColor(Color(red: 224/255, green: 152/255, blue: 56/255))
+                .frame(width: 685, height: 685)
+                .offset(y: -200) // Masih nembak, kalo di Sketch -137
+            VStack {
+                Text("Let's stretch!")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                Text("- or not. We don't really care.")
+                    .foregroundColor(.white)
+                
+                ZStack {
+                    if isTimerVisible{
+                        Text("20")
+                            .font(.system(size: 280, weight: .thin))
+                            .foregroundColor(.white)
+                            .offset(y: -30)
+                        // Dummy clown
+                        Image("dummy")
+                            .scaleEffect(1.5)
+                            .offset(y: 100)
+                            
+                    }
+                }
+                
+                Spacer()
+                
+                
+                if !isTimerVisible {
+                    Button("Chop chop!"){
+                        withAnimation(.easeIn){
+                            isTimerVisible.toggle()
+                        }
+                    }
+                    .foregroundColor(Color(red: 16/255, green: 38/255, blue: 67/255))
+                    .fontWeight(.semibold)
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 32)
+                    .background(.white)
+                    .cornerRadius(8)
+                }
+                
+            }
+            .padding()
+        }
+        .background(Color(red: 16/255, green: 38/255, blue: 67/255))
+    }
+}
+
+struct StretchPage_Previews: PreviewProvider {
+    static var previews: some View {
+        StretchPage()
+    }
+}
