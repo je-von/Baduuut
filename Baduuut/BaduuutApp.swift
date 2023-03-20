9196a72fba61f4e44a2c1910bbae12b4df1b8177
leo <leoharnadi@gmail.com>
2023-03-20 13:14:37 +0700

    Initial Commit

---
+++ Baduuut/BaduuutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaduuutApp.swift
+//  Baduuut
+//
+//  Created by Leo Harnadi on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BaduuutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baduuut/CircularTImerView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularTImerView.swift
+//  Baduuut
+//
+//  Created by Leo Harnadi on 20/03/23.
+//
+
+import SwiftUI
+
+struct CircularTImerView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct CircularTImerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularTImerView()
+    }
+}
