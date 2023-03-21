48b21a062cbb0049364fcd5a9b619d37c291c581
je-von <jevon.levin@gmail.com>
2023-03-21 17:38:46 +0700

    feat: show bottom sheet

--- Baduuut/OnboardPage.swift
+++ Baduuut/OnboardPage.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Lottie
 struct OnboardPage: View {
+    @State private var isSheetVisible = false
     @State private var hasStarted = false
     @State private var selectedTime = "Every 30 minutes"
     let times = ["Every 15 seconds", "Every 30 minutes", "Every 1 hour", "Every 2 hours"]
@@ -28,7 +29,7 @@ struct OnboardPage: View {
             
             //fix: tulisannya, warna button?
             Button("Set Session"){
-                
+                isSheetVisible = true
             }
             .font(.system(size: 18))
             .fontWeight(.semibold)
@@ -37,6 +38,43 @@ struct OnboardPage: View {
             .foregroundColor(Color(red: 16/255, green: 38/255, blue: 67/255))
             .background(.white)
             .cornerRadius(8)
+            .sheet(isPresented: $isSheetVisible) {
+                VStack(spacing: .none){
+                    Text("How often should I bug you?")
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                        .bold()
+                        .fixedSize(horizontal: false, vertical: true)
+                        .foregroundColor(Color(red: 16/255, green: 38/255, blue: 67/255))
+                    
+                    Picker("Time", selection: $selectedTime) {
+                        ForEach(times, id: \.self) {
+                            Text($0)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color(red: 224/255, green: 152/255, blue: 56/255))
+                            
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    .frame(height: 120)
+                    .scaleEffect(1.1)
+                    .padding(.bottom, -5)
+                    .padding(.top, -17)
+                    
+                    Button("Start!"){
+                        isSheetVisible = false
+                        hasStarted = true
+                    }
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 32)
+                    .background(Color(red: 16/255, green: 38/255, blue: 67/255))
+                    .cornerRadius(8)
+                    
+                }
+                .presentationDetents([.height(260)])
+            }
             
             NavigationLink("Stretch Now"){
                 StretchPage()
@@ -51,43 +89,7 @@ struct OnboardPage: View {
         }
     }
 }
-//VStack(spacing: .none){
-//    Text("How often should I bug you?")
-//        .multilineTextAlignment(.center)
-//        .font(.title2)
-//        .bold()
-//        .fixedSize(horizontal: false, vertical: true)
-//        .foregroundColor(.white)
-//
-//    Picker("Time", selection: $selectedTime) {
-//        ForEach(times, id: \.self) {
-//            Text($0)
-//                .fontWeight(.medium)
-//                .foregroundColor(Color(red: 224/255, green: 152/255, blue: 56/255))
-//
-//        }
-//    }
-//    .pickerStyle(.wheel)
-//    .frame(height: 120)
-//    .scaleEffect(1.1)
-//    .padding(.bottom, -5)
-//    .padding(.top, -17)
-//
-//    Button("Start!"){
-//        withAnimation{
-//            hasStarted = true
-//        }
-//    }
-//    .foregroundColor(Color(red: 16/255, green: 38/255, blue: 67/255))
-//    .fontWeight(.semibold)
-//    .padding()
-//    .frame(width: UIScreen.main.bounds.width - 32)
-//    .background(.white)
-//    .cornerRadius(8)
-//
-//}
-//.frame(width: UIScreen.main.bounds.width - 32)
-//
+
 
 struct OnboardPage_Previews: PreviewProvider {
     static var previews: some View {
